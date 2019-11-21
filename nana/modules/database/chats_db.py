3ec0cc8622707eeeecf7174b68bc661393a01ff8
Ayra Hikari <ayrahikari@linuxmail.org>
2019-11-21 19:47:15 +0700

    Added logs for self admin

--- nana/modules/database/chats_db.py
+++ nana/modules/database/chats_db.py
@@ -18,10 +18,30 @@ class MyChats(BASE):
 	def __repr__(self):
 		return "<Chat {} ({})>".format(self.chat_name, self.chat_id)
 
+class MyChatsAdmin(BASE):
+	__tablename__ = "my_chats_admin"
+	chat_id = Column(String(14), primary_key=True)
+	chat_name = Column(UnicodeText, nullable=False)
+	chat_username = Column(UnicodeText)
+	chat_status = Column(UnicodeText)
+
+	def __init__(self, chat_id, chat_name, chat_username, chat_status):
+		self.chat_id = str(chat_id)
+		self.chat_name = chat_name
+		self.chat_username = chat_username
+		self.chat_status = chat_status
+
+	def __repr__(self):
+		return "<Chat Admin {} ({})>".format(self.chat_name, self.chat_id)
+
 MyChats.__table__.create(checkfirst=True)
+MyChatsAdmin.__table__.create(checkfirst=True)
 
 
 MY_ALL_CHATS = {}
+MY_ALL_ADMINS = {}
+MY_CHATS_CREATOR = {}
+MY_CHATS_ADMINS = {}
 
 
 def update_chat(chat):
@@ -37,12 +57,56 @@ def update_chat(chat):
 	SESSION.commit()
 	MY_ALL_CHATS[chat.id] = {"name": chat.title, "username": chat.username}
 
+def update_chat_admin(chat, status):
+	global MY_ALL_ADMINS, MY_CHATS_CREATOR, MY_CHATS_ADMINS
+	if chat.id in list(MY_ALL_ADMINS):
+		if MY_ALL_ADMINS.get(chat.id) and MY_ALL_ADMINS[chat.id].get('name') == chat.title and MY_ALL_ADMINS[chat.id].get('username') == chat.username:
+			return
+	chat_db = SESSION.query(MyChatsAdmin).get(str(chat.id))
+	if chat_db:
+		SESSION.delete(chat_db)
+	chat_db = MyChatsAdmin(str(chat.id), chat.title, chat.username, status)
+	SESSION.add(chat_db)
+	SESSION.commit()
+	MY_ALL_ADMINS[chat.id] = {"name": chat.title, "username": chat.username}
+	if status == "creator":
+		MY_CHATS_CREATOR[chat.id] = {"name": chat.title, "username": chat.username}
+	if status == "administrator":
+		MY_CHATS_ADMINS[chat.id] = {"name": chat.title, "username": chat.username}
+
 def get_all_chats():
 	try:
 		return SESSION.query(MyChats).all()
 	finally:
 		SESSION.close()
 
+def get_all_chats_admin():
+	try:
+		return SESSION.query(MyChatsAdmin).all()
+	finally:
+		SESSION.close()
+
+def get_all_chats_creator():
+	try:
+		MY_CREATOR = {}
+		qall = SESSION.query(MyChatsAdmin).all()
+		for x in qall:
+			if x.chat_status == "creator":
+				MY_CREATOR[x.chat_id] = {"name": x.chat_name, "username": x.chat_username}
+		return MY_CREATOR
+	finally:
+		SESSION.close()
+
+def get_all_chats_admin_only():
+	try:
+		MY_ADMIN = {}
+		qall = SESSION.query(MyChatsAdmin).all()
+		for x in qall:
+			if x.chat_status == "administrator":
+				MY_ADMIN[x.chat_id] = {"name": x.chat_name, "username": x.chat_username}
+		return MY_ADMIN
+	finally:
+		SESSION.close()
 
 def __load_mychats():
 	global MY_ALL_CHATS
@@ -54,4 +118,24 @@ def __load_mychats():
 	finally:
 		SESSION.close()
 
+def __load_mychats_admin():
+	global MY_ALL_ADMINS, MY_CHATS_CREATOR, MY_CHATS_ADMINS
+	try:
+		MY_ALL_ADMINS = {}
+		MY_CHATS_CREATOR = {}
+		MY_CHATS_ADMINS = {}
+		qall = SESSION.query(MyChatsAdmin).all()
+		for x in qall:
+			MY_ALL_ADMINS[x.chat_id] = {"name": x.chat_name, "username": x.chat_username}
+			if x.chat_status == "creator":
+				MY_CHATS_CREATOR[x.chat_id] = {"name": x.chat_name, "username": x.chat_username}
+			if x.chat_status == "administrator":
+				MY_CHATS_ADMINS[x.chat_id] = {"name": x.chat_name, "username": x.chat_username}
+		print(MY_ALL_ADMINS)
+		print(MY_CHATS_CREATOR)
+		print(MY_CHATS_ADMINS)
+	finally:
+		SESSION.close()
+
 __load_mychats()
+__load_mychats_admin()
